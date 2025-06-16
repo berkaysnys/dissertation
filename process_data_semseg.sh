4e0e6e536b0555804a4aa081e7bd278399cf90d5
berkaysnys <150937548+berkaysnys@users.noreply.github.com>
2025-06-17 01:00:38 +0300

    Update process_data_semseg.sh

--- process_data_semseg.sh
+++ process_data_semseg.sh
@@ -1,2 +1,2 @@
-python3 content/dissertation/dataloader/semseg/process_belhouse3d.py --config 'content/dissertation/dataloader/semseg/process_belhouse3d_iid.yaml'
-python3 content/dissertation/dataloader/semseg/process_belhouse3d.py --config 'content/dissertation/dataloader/semseg/process_belhouse3d_ood.yaml'
+python3 /content/dissertation/dataloader/semseg/process_belhouse3d.py --config '/content/dissertation/dataloader/semseg/process_belhouse3d_iid.yaml'
+python3 /content/dissertation/dataloader/semseg/process_belhouse3d.py --config '/content/dissertation/dataloader/semseg/process_belhouse3d_ood.yaml'
